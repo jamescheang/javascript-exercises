cbe08608d0b289a7aeb8bbc92d90f3892b6727eb
jamescheang <cheang.james@gmail.com>
2023-02-15 12:46:58 +0800

    03 - js completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (textToReverse) {
+    const textArray = textToReverse.split("");
+    const reverseArray = textArray.reverse();
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
