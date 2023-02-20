0126447e028a73c3b22aebb7d5b521f6eff157c9
jamescheang <cheang.james@gmail.com>
2023-02-20 10:13:16 +0800

    04 - js completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    let filteredArray = arr;
 
+    for (let i = 1; i < arguments.length; i++) {
+        console.log("argument " + i + ": " + arguments[i]);
+        filteredArray = filteredArray.filter(x => x !== arguments[i])
+        console.log(filteredArray);
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
