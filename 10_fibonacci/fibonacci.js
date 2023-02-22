562ff6151e2771766eca12560ee9f073b2dc4d16
jamescheang <cheang.james@gmail.com>
2023-02-22 15:16:19 +0800

    10 - js completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let final = 1;
+    let current = 1;
+    let previous = 0;
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 0; i < num - 2; i++) {
+        previous = current;
+        current = final;
+        final = previous + current;
+    }
+
+    return final;
 };
 
 // Do not edit below this line
