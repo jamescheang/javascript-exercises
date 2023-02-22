3a6746c992e5abbf5e71c7fd67a62ac2e98ef91d
jamescheang <cheang.james@gmail.com>
2023-02-22 14:20:42 +0800

    08 - js completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (firstN, secondN) {
+  return firstN + secondN;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstN, secondN) {
+  return firstN - secondN;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let final = 0;
+  for (i = 0; i < arr.length; i++) {
+    final += arr[i];
+  }
+  return final;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let final = 1;
+  for (i = 0; i < arr.length; i++) {
+    final *= arr[i];
+  }
+  return final;
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  let final = num;
+  for (let i = 1; i < pow; i++) {
+    final *= num;
+  }
+
+  return final;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let final = 1;
+  for (let i = 1; i <= num; i++) {
+    final *= i;
+  }
+
+  return final;
 };
 
 // Do not edit below this line
