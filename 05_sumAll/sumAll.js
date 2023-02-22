c65443c9f859a126703ee2737c293a0b8b70182a
jamescheang <cheang.james@gmail.com>
2023-02-22 12:04:43 +0800

    05 - js completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function () {
+    let start = arguments[0];
+    let end = arguments[1];
+    let finalSum = 0;
+
+    if (typeof (start) != 'number' || typeof (end) != 'number') {
+        return 'ERROR';
+    }
+
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    if (end < start) {
+        start = arguments[1];
+        end = arguments[0];
+    }
+
+    for (let i = start; i <= end; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 
 };
 
